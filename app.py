adfaba64883ca987e5f2904c429999e746740a1c
369696807 <369696807@qq.com>
2017-11-26 01:55:50 +0800

    version 0.0.1 backup

--- app.py
+++ app.py
@@ -15,20 +15,33 @@ class ArticleList(object):
 		self.article_list = self.update_article_list()
 
 	def self.get_article_list(self):
-		return self.article_list 
+		return self.load_article_list_json()
 
+	def self.get_article_list_page(self, page_num=1, item_num_per_page=10)
+
+		return self.load_article_list_json()
 	def self.load_article_list_json(self):
 		"""
 		load the existing article_list.json file 
-		if it doesn't exists, create a new one
+		if it doesn't exists or it is out-of-date create a new one
 		"""
 		if not os.path.isfile(self.article_list_json_path):
-			self.article_list = self.update_article_list()
+			return self.update_article_list()
 		else:
 			# load json
 			try:
 				with open(self.article_list_json_path, 'r') as list_file:
-					self.article_list = json.loads(list_file.read())
+					new_article_list = json.loads(list_file.read())
+
+				# checkout last_update_time
+				time_now = datetime.now()
+				_last_time_str = self.article_list[-1]['last_update_time']
+				last_time = datetime.strptime(_last_time_str, '%Y-%m-%d %H:%M:%S')
+				# if it is out-of-date
+				if (time_now - last_time).totalseconds() > 300:
+					return self.update_article_list()
+				else:
+					new_article_list
 			except IOError:
 				abort(404)
 				return 
@@ -45,7 +58,7 @@ class ArticleList(object):
 			
 			# add time_stamp
 			time_stamp = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-			new_article_list.append({'list_update_time':time_now_str})
+			new_article_list.append({'last_update_time':time_now_str})
 
 			# dump json to file 
 			with open(self.article_list_json_path, 'r') as list_file:
@@ -60,8 +73,6 @@ class ArticleList(object):
 		for js in json_list:
 			with open('{}{}'.format(self.article_json_dir, js), 'r') as json_file:
 				article = json.loads(json_file.read())
-				_title = article['title']
-				_created_time = 
 				article_info_list.append({'title':article['title'], 
 										'file_name':js, 
 										'created_time':article['created_time']})
@@ -79,11 +90,11 @@ class ArticleList(object):
 			return 
 
 	def self._sort_json_list_by_time(self, j_list):
-		j_list.sort(cmp=self.datetime_cmp, key=operator.itemgetter('created_time'))
+		j_list.sort(cmp=self._datetime_cmp, key=operator.itemgetter('created_time'))
 		return j_list
 
 	@staticmethod
-	def self.datetime_cmp(a, b):
+	def self._datetime_cmp(a, b):
 		a_dt = datetime.strptime(a, '%Y-%m-%d %H:%M:%S')
 		b_dt = datetime.strptime(b, '%Y-%m-%d %H:%M:%S')
 		if a_dt > b_dt:
@@ -95,11 +106,41 @@ class ArticleList(object):
 
 class Article(object):
 	"""
-	
+	"""
+	self.article_json_dir = '/home/shiyanlou/files'
+
+	def self. __init__(self, filename):
+		self.filename = filename
+		self.article_item = self.get_article_json(filename)
+
+	def self.get_article_json(self, filename):
+		file_path = self.article_json_dir + self.get_article_file_name
+		with open(file_path, 'r') as json_file:
+			return json.loads(json_file.read())
+
+	def self.get_file_name(self):
+		return self.filename
+
+	def self.get_article_title(self):
+		return self.artitle_item['title']
+
+	def self.get_article_created_time:(self):
+		return self.artitle_item['created_time']
+
+	def self.get_article_content:(self):
+		return self.article_item['content']
+
+alist = ArticleList()
 
 @app.route('/')
 def index():
-	 
+	home_index = alist.get_article_list()
 
 @app.route('/files/<filename>')
 def file(filename):
+	article = Article(filename)
+	return render_template('file.html', 
+							title=article.get_article_title,
+							created_time=article.get_article_created_time,
+							content=article.get_article_content
+							)
