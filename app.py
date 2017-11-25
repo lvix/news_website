ab0b43b66b8c40fe5cd2eaa85d58f97ee3be3c0c
369696807 <369696807@qq.com>
2017-11-26 04:56:53 +0800

    version 0.0.1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, abort
 from datetime import datetime
 import os, json, operator
 # classes
@@ -8,19 +8,19 @@ class ArticleList(object):
 	"""
 	display the jason files in ~/files/
 	"""
-	self.article_list_json_path = '/home/shiyanlou/news/article_list.json'
-	self.article_json_dir = '/home/shiyanlou/files'
+	article_list_json_path = '/home/shiyanlou/news/article_list.json'
+	article_json_dir = '/home/shiyanlou/files'
 
-	def self.__init__(self):
+	def __init__(self):
+		# print('start!')
 		self.article_list = self.update_article_list()
+		# print(self.get_article_list())
 
-	def self.get_article_list(self):
-		return self.load_article_list_json()
+	def get_article_list(self):
+		art_list = self.load_article_list_json()
+		return art_list[:-1]
 
-	def self.get_article_list_page(self, page_num=1, item_num_per_page=10)
-
-		return self.load_article_list_json()
-	def self.load_article_list_json(self):
+	def load_article_list_json(self):
 		"""
 		load the existing article_list.json file 
 		if it doesn't exists or it is out-of-date create a new one
@@ -38,63 +38,69 @@ class ArticleList(object):
 				_last_time_str = self.article_list[-1]['last_update_time']
 				last_time = datetime.strptime(_last_time_str, '%Y-%m-%d %H:%M:%S')
 				# if it is out-of-date
-				if (time_now - last_time).totalseconds() > 300:
+				if (time_now - last_time).total_seconds() > 300:
 					return self.update_article_list()
 				else:
-					new_article_list
+					return new_article_list
 			except IOError:
-				abort(404)
+				# abort(404)
 				return 
 
-	def self.update_article_list(self):
-		json_list = self._get_json_file_dirlist()
-		
+	def update_article_list(self):
+		json_file_list = self._get_json_file_dirlist()
+		# print(json_file_list)
 		try:
 			# read basic information in article jsons
-			new_article_list = self._read_json_basic_info
-
+			new_article_list = self._read_json_basic_info(json_file_list)
 			# sort
 			new_article_list = self._sort_json_list_by_time(new_article_list)
 			
 			# add time_stamp
 			time_stamp = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-			new_article_list.append({'last_update_time':time_now_str})
+			new_article_list.append({'last_update_time':time_stamp})
 
 			# dump json to file 
-			with open(self.article_list_json_path, 'r') as list_file:
+			# print('dump:')
+			# print(self.article_list_json_path)
+			with open(self.article_list_json_path, 'w') as list_file:
 				list_file.write(json.dumps(new_article_list))
 
 			return new_article_list
-		except:IOError
+		except IOError:
 			return 
 
-	def self._read_json_basic_info(self, json_list):
+	def _read_json_basic_info(self, json_file_list):
 		article_info_list = []
-		for js in json_list:
-			with open('{}{}'.format(self.article_json_dir, js), 'r') as json_file:
-				article = json.loads(json_file.read())
-				article_info_list.append({'title':article['title'], 
-										'file_name':js, 
-										'created_time':article['created_time']})
-		return article_info_list
-
-	def self._get_json_file_dirlist(self):
+		try:
+			for js in json_file_list:
+				json_path = '{}/{}.json'.format(self.article_json_dir, js)
+				# print(json_path)
+				with open(json_path, 'r') as json_file:
+					article = json.loads(json_file.read())
+					article_info_list.append({'title':article['title'], 
+											'file_name':js, 
+											'created_time':article['created_time']})
+			return article_info_list
+		except:
+			# print('cannot read jsons')
+			return 
+	def _get_json_file_dirlist(self):
 		try:
 			file_list = os.listdir(self.article_json_dir)
 			json_list = []
 			for f_name in file_list:
 				if f_name[-5:] == '.json':
-					json_list.append(f_name)
+					json_list.append(f_name[:-5])
 			return json_list
 		except:
 			return 
 
-	def self._sort_json_list_by_time(self, j_list):
-		j_list.sort(cmp=self._datetime_cmp, key=operator.itemgetter('created_time'))
+	def _sort_json_list_by_time(self, j_list):
+		j_list.sort(key=operator.itemgetter('created_time'))
 		return j_list
 
 	@staticmethod
-	def self._datetime_cmp(a, b):
+	def _datetime_cmp(a, b):
 		a_dt = datetime.strptime(a, '%Y-%m-%d %H:%M:%S')
 		b_dt = datetime.strptime(b, '%Y-%m-%d %H:%M:%S')
 		if a_dt > b_dt:
@@ -107,40 +113,62 @@ class ArticleList(object):
 class Article(object):
 	"""
 	"""
-	self.article_json_dir = '/home/shiyanlou/files'
+	article_json_dir = '/home/shiyanlou/files/'
 
-	def self. __init__(self, filename):
+	def __init__(self, filename):
 		self.filename = filename
-		self.article_item = self.get_article_json(filename)
+		self.article_item = self.read_article_json()
 
-	def self.get_article_json(self, filename):
-		file_path = self.article_json_dir + self.get_article_file_name
+	def read_article_json(self):
+		file_path = self.article_json_dir + self.get_file_name() + '.json'
+		# print(file_path)
 		with open(file_path, 'r') as json_file:
 			return json.loads(json_file.read())
 
-	def self.get_file_name(self):
+	def get_file_name(self):
 		return self.filename
 
-	def self.get_article_title(self):
-		return self.artitle_item['title']
+	def get_article_title(self):
+		return self.article_item['title']
 
-	def self.get_article_created_time:(self):
-		return self.artitle_item['created_time']
+	def get_article_created_time(self):
+		return self.article_item['created_time']
 
-	def self.get_article_content:(self):
-		return self.article_item['content']
+	def get_article_content(self):
+		content_ori = self.article_item['content']
+		# print(content_ori)
+		content = []
+		if r'\\n' in content_ori:
+			content = content_ori.split(r'\\n')
+		elif r'\n' in content_ori:
+			content = content_ori.split(r'\n')
+		return content
 
 alist = ArticleList()
 
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+	return render_template('404.html'), 404
+
 @app.route('/')
 def index():
-	home_index = alist.get_article_list()
+	try:
+		items = alist.get_article_list()
+	except:
+		abort(404)
+	# print(items)
+	return	render_template('index.html', items=items)
 
 @app.route('/files/<filename>')
 def file(filename):
-	article = Article(filename)
-	return render_template('file.html', 
-							title=article.get_article_title,
-							created_time=article.get_article_created_time,
-							content=article.get_article_content
-							)
+	try:
+		article = Article(filename)
+	except:
+		abort(404)
+	title = article.get_article_title()
+	created_time = article.get_article_created_time()
+	content = article.get_article_content()
+	# print(content)
+	return render_template('file.html', title=title, created_time=created_time, content=content)
