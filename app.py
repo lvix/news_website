cfadd5131cda678f85df8138ab542ad1a8161142
369696807 <369696807@qq.com>
2017-11-29 05:58:53 +0800

    version 0.0.3 with MongoDB

--- app.py
+++ app.py
@@ -2,157 +2,16 @@
 # -*- coding:utf-8 -*-
 from flask import Flask, request, render_template, abort
 from flask_sqlalchemy import SQLAlchemy 
+from pymongo import MongoClient
 from datetime import datetime
 import os, json, operator
-
+# MySQL
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/news'
 db = SQLAlchemy(app)
-
-# classes
-class ArticleList(object):
-	"""
-	display the jason files in ~/files/
-	"""
-	article_list_json_path = '/home/shiyanlou/news/article_list.json'
-	article_json_dir = '/home/shiyanlou/files'
-
-	def __init__(self):
-		# print('start!')
-		self.article_list = [] #load_articles_from_db()
-		# self.update_article_list()
-		# print(self.get_article_list())
-	def load_articles_from_db():
-		pass
-
-	def get_article_list(self):
-		art_list = []
-		# self.load_article_list_json()
-		return art_list[:-1]
-
-	def load_article_list_json(self):
-		"""
-		load the existing article_list.json file 
-		if it doesn't exists or it is out-of-date create a new one
-		"""
-		if not os.path.isfile(self.article_list_json_path):
-			return self.update_article_list()
-		else:
-			# load json
-			try:
-				with open(self.article_list_json_path, 'r') as list_file:
-					new_article_list = json.loads(list_file.read())
-
-				# checkout last_update_time
-				time_now = datetime.now()
-				_last_time_str = self.article_list[-1]['last_update_time']
-				last_time = datetime.strptime(_last_time_str, '%Y-%m-%d %H:%M:%S')
-				# if it is out-of-date
-				if (time_now - last_time).total_seconds() > 300:
-					return self.update_article_list()
-				else:
-					return new_article_list
-			except IOError:
-				# abort(404)
-				return 
-
-	def update_article_list(self):
-		json_file_list = self._get_json_file_dirlist()
-		# print(json_file_list)
-		try:
-			# read basic information in article jsons
-			new_article_list = self._read_json_basic_info(json_file_list)
-			# sort
-			new_article_list = self._sort_json_list_by_time(new_article_list)
-			
-			# add time_stamp
-			time_stamp = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
-			new_article_list.append({'last_update_time':time_stamp})
-
-			# dump json to file 
-			# print('dump:')
-			# print(self.article_list_json_path)
-			with open(self.article_list_json_path, 'w') as list_file:
-				list_file.write(json.dumps(new_article_list))
-
-			return new_article_list
-		except IOError:
-			return 
-
-	def _read_json_basic_info(self, json_file_list):
-		article_info_list = []
-		try:
-			for js in json_file_list:
-				json_path = '{}/{}.json'.format(self.article_json_dir, js)
-				# print(json_path)
-				with open(json_path, 'r') as json_file:
-					article = json.loads(json_file.read())
-					article_info_list.append({'title':article['title'], 
-											'file_name':js, 
-											'created_time':article['created_time']})
-			return article_info_list
-		except:
-			# print('cannot read jsons')
-			return 
-	def _get_json_file_dirlist(self):
-		try:
-			file_list = os.listdir(self.article_json_dir)
-			json_list = []
-			for f_name in file_list:
-				if f_name[-5:] == '.json':
-					json_list.append(f_name[:-5])
-			return json_list
-		except:
-			return 
-
-	def _sort_json_list_by_time(self, j_list):
-		j_list.sort(key=operator.itemgetter('created_time'))
-		return j_list
-
-	@staticmethod
-	def _datetime_cmp(a, b):
-		a_dt = datetime.strptime(a, '%Y-%m-%d %H:%M:%S')
-		b_dt = datetime.strptime(b, '%Y-%m-%d %H:%M:%S')
-		if a_dt > b_dt:
-			return 1
-		elif a_dt < b_dt:
-			return -1 
-		else:
-			return 0 
-
-class Article(object):
-	"""
-	"""
-	article_json_dir = '/home/shiyanlou/files/'
-
-	def __init__(self, filename):
-		self.filename = filename
-		self.article_item = None # self.read_article_json()
-
-	def read_article_json(self):
-		file_path = self.article_json_dir + self.get_file_name() + '.json'
-		# print(file_path)
-		with open(file_path, 'r') as json_file:
-			return json.loads(json_file.read())
-
-	def get_file_name(self):
-		return self.filename
-
-	def get_article_title(self):
-		return self.article_item['title']
-
-	def get_article_created_time(self):
-		return self.article_item['created_time']
-
-	def get_article_content(self):
-		content_ori = self.article_item['content']
-		# print(content_ori)
-		content = []
-		if r'\\n' in content_ori:
-			content = content_ori.split(r'\\n')
-		elif r'\n' in content_ori:
-			content = content_ori.split(r'\n')
-		return content
+# MongoDB
+client = MongoClient('127.0.0.1', 27017)
+mdb = client.news
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -175,6 +34,49 @@ class Post(db.Model):
 	def __repr__(self):
 		return '<Post %r>' % self.title 
 
+	# add tag to a post
+	def add_tag(self, tag_name):
+		# add a Tag called tag_name to current Post in MongoDB
+		try:
+			tags = self.tags
+			# print('tags:', tags )
+			if tag_name not in tags:
+				tags.append(tag_name)
+				# print(new_tags)
+				mdb.news.update({'id':self.id}, {'$set':{'tags':tags}})
+		except:
+			print('cannot add tag')
+			return
+
+	# remove tag from a post
+	def remove_tag(self, tag_name):
+		# remove a Tag called tag_name from current Post in MongoDB
+		try:
+			tags = self.tags
+			if tag_name in tags:
+				tags.remove(tag_name)
+				mdb.news.update({'id':self.id}, {'$set':{'tags':tags}})
+		except:
+			return
+
+	# return current Post's  tags
+	@property 
+	def tags(self):
+		try:
+			m_post = mdb.news.find_one({'id':self.id})
+			if m_post == None:
+				new_tags = []
+				new_post = {'id':self.id, 'tags':new_tags}
+				mdb.news.insert_one(new_post)
+				# print('insert one ', new_post)
+				return new_tags
+			else:
+				# print('check exising tags', m_post)
+				return m_post['tags']
+		except:
+			print('get tags error')
+			return []
+
 
 class Category(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -187,13 +89,12 @@ class Category(db.Model):
 		return '<Category %r>' % self.name 
 
 
-alist = ArticleList()
-def get_article_items_from_db():
+def get_post_items_from_db():
 	plist = db.session.query(Post).all()
 	nlist = []
 	for po in plist:
 		_ctime = datetime.strftime(po.created_time, '%Y-%m-%d %H:%M:%S')
-		p_item = {'file_name':po.id, 'title':po.title, 'created_time':_ctime}
+		p_item = {'file_name':po.id, 'title':po.title, 'created_time':_ctime, 'tags':po.tags}
 		nlist.append(p_item)
 	return nlist 
 
@@ -205,30 +106,23 @@ def not_found(error):
 @app.route('/')
 def index():
 	try:
-		# items = alist.get_article_list()
-		items = get_article_items_from_db()
+		posts = get_post_items_from_db()
 	except:
 		abort(404)
-	# print(items)
-	return	render_template('index.html', items=items)
+	return	render_template('index.html', items=posts)
 
 @app.route('/files/<filename>')
 def file(filename):
-	# try:
-		# article = Article(filename)
-	# except:
-		# abort(404)
 	try:
-		article = db.session.query(Post).filter(Post.id==str(filename)).first()
+		post = db.session.query(Post).filter(Post.id==str(filename)).first()
 	except:
 		abort(404)
-	if not article:
+	if not post:
 		abort(404)
-	title = article.title
-	created_time = datetime.strftime(article.created_time, '%Y-%m-%d %H:%M:%S')
-	print(type(article.content))
-	print(article.content)
-	content = article.content
 
-	# print(content)
+	# set variables
+	title = post.title
+	created_time = datetime.strftime(post.created_time, '%Y-%m-%d %H:%M:%S')
+	content = post.content
+	# tags = post.tags
 	return render_template('file.html', title=title, created_time=created_time, content=content)
---
+++ init_test_data.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env/ python3
+
+from app import db, Post, Category
+from datetime import datetime
+
+
+db.create_all()
+# # ?? MySQL ????
+
+java = Category('Java')
+python = Category('Python')
+post1 = Post('Hello Java', java, 'File Content - Java is cool!', datetime.utcnow())
+post2 = Post('Hello Python', python, 'File Content - Python is cool!', datetime.utcnow())
+db.session.add(java)
+db.session.add(python)
+db.session.add(post1)
+db.session.add(post2)
+db.session.commit()
+
+# ?? MongoDB ????
+# client = MongoClient('127.0.0.1', 27017)
+# mdb = client.news
+post1.add_tag('tech')
+post1.add_tag('java')
+post1.add_tag('linux')
+post2.add_tag('tech')
+post2.add_tag('python')
